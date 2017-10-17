df53cc30f1552313f01db4986a05437874f596e0
Hwanghyohyuk <hhhwang618@naver.com>
2017-10-17 23:09:22 +0900

    11

--- Project Workspace/TestProject/src/com/studyhub/admin/recruitmanagement/model/service/RecruitManagementService.java
+++ Project Workspace/TestProject/src/com/studyhub/admin/recruitmanagement/model/service/RecruitManagementService.java
@@ -1,5 +1,13 @@
 package com.studyhub.admin.recruitmanagement.model.service;
 
+import com.studyhub.admin.recruitmanagement.model.dao.RecruitManagementDao;
+import com.studyhub.common.vo.Board;
+
 public class RecruitManagementService {
 
+	private RecruitManagementDao rmd;
+	private Board board;
+
+	
+	
 }
--- Project Workspace/TestProject/src/com/studyhub/admin/usermanagement/model/service/UserManagementService.java
+++ Project Workspace/TestProject/src/com/studyhub/admin/usermanagement/model/service/UserManagementService.java
@@ -1,5 +1,11 @@
 package com.studyhub.admin.usermanagement.model.service;
 
+import com.studyhub.admin.usermanagement.model.dao.UserManagementDao;
+import com.studyhub.common.vo.User;
+
 public class UserManagementService {
 
+	private UserManagementDao umd;
+	private User user;
+	
 }
