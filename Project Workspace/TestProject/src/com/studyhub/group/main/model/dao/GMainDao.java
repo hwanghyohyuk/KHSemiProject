f232243539c96169225f07fa8f2d01d496535302
ydk0831 <31609765+ydk0831@users.noreply.github.com>
2017-10-24 22:53:25 +0900

    dk

--- Project Workspace/TestProject/src/com/studyhub/group/main/model/dao/GMainDao.java
+++ Project Workspace/TestProject/src/com/studyhub/group/main/model/dao/GMainDao.java
@@ -23,28 +23,24 @@ public class GMainDao {
 	private GQNA gQna;
 	private Schedule schedule;
 	private ShareFile shareFile;
-	
-	public Group selectGroup(Connection con, int group_no, int user_no) {
+
+	public Group selectGroup(Connection con, int group_no) {
 		group = null;
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		
-		String query = "select group_no ,group_name, attribute_name, location, category_name, description, authority_no"
-					+ " from tb_group"
-					+ " join tb_on_off using(attribute_no)"
-					+ " join tb_category using(category_no)"
-					+ " join (select group_no, authority_no from tb_ung where user_no=?) using(group_no)"
-					+ " where group_no = ?";
-		
+
+		String query = "select group_no ,group_name, attribute_name, location, category_name, description"
+				+ " from tb_group g" + " join tb_on_off using(attribute_no)" + " join tb_category using(category_no)"
+				+ " where g.group_no = ?";
+
 		try {
 			pstmt = con.prepareStatement(query);
-			pstmt.setInt(1, user_no);
-			pstmt.setInt(2, group_no);
-			
+			pstmt.setInt(1, group_no);
+
 			rset = pstmt.executeQuery();
-			if(rset.next()){
+			if (rset.next()) {
 				group = new Group();
-				
+
 				group.setGroupNo(group_no);
 				group.setGroupName(rset.getString("group_name"));
 				group.setAttributeName(rset.getString("attribute_name"));
@@ -59,40 +55,31 @@ public class GMainDao {
 			close(rset);
 			close(pstmt);
 		}
-		
+
 		return group;
 	}
 
-
 	public Group SelectGroupMain(Connection con, int group_no) {
 		Group g = null;
-		
+
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		
-		String query = 	"select group_no, user_name, membercount, category_name, location, attribute_name " +
-						"from tb_group " +
-						"join (select group_no, user_name " +
-						"from tb_ung " +
-						"join tb_user using(user_no) " +
-						"where authority_no = 2) using (group_no) " +
-						"join (select group_no, category_name " +
-						"from tb_group " +
-						"join tb_category using (category_no)) using(group_no) " +
-						"join (select group_no, attribute_name " +
-						"from tb_on_off " +
-						"join tb_group using(attribute_no)) using(group_no) " +
-						"join (select group_no, count(*) as membercount " +
-						"from tb_ung " +
-						"group by group_no) using(group_no) " +
-						"where group_no = ?";
-		
+
+		String query = "select group_no, user_name, membercount, category_name, location, attribute_name "
+				+ "from tb_group " + "join (select group_no, user_name " + "from tb_ung "
+				+ "join tb_user using(user_no) " + "where authority_no = 2) using (group_no) "
+				+ "join (select group_no, category_name " + "from tb_group "
+				+ "join tb_category using (category_no)) using(group_no) " + "join (select group_no, attribute_name "
+				+ "from tb_on_off " + "join tb_group using(attribute_no)) using(group_no) "
+				+ "join (select group_no, count(*) as membercount " + "from tb_ung "
+				+ "group by group_no) using(group_no) " + "where group_no = ?";
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, group_no);
-			
+
 			rset = pstmt.executeQuery();
-			if(rset.next()) {
+			if (rset.next()) {
 				g = new Group();
 				g.setGroupNo(rset.getInt("group_no"));
 				g.setUserName(rset.getString("user_name"));
@@ -110,26 +97,24 @@ public class GMainDao {
 		return g;
 	}
 
-
 	public ArrayList<GNotice> selectGroupNotice(Connection con, int groupno) {
-	ArrayList<GNotice> list = null;
-		
+		ArrayList<GNotice> list = null;
+
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		
-		String query = "select notice_no, title, uploader, upload_date" +
-					" from tb_g_notice" +
-					" join tb_user on (tb_g_notice.uploader=tb_user.user_no)" +
-					" where notice_no = ?";
-		
+
+		String query = "select notice_no, title, uploader, upload_date" + " from tb_g_notice"
+				+ " join tb_user on (tb_g_notice.uploader=tb_user.user_no)" + " where notice_no = ?";
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, groupno);
-			
+
 			rset = pstmt.executeQuery();
-			if(rset != null){
+			if (rset != null) {
 				list = new ArrayList<GNotice>();
-				while(rset.next());
+				while (rset.next())
+					;
 				GNotice gn = new GNotice();
 				gn.setNoticeNo(rset.getInt("notice_no"));
 				gn.setTitle(rset.getString("title"));
@@ -146,25 +131,23 @@ public class GMainDao {
 		}
 		return list;
 	}
-	
+
 	public ArrayList<GBoard> selectGroupBoard(Connection con, int groupno) {
 		ArrayList<GBoard> list = null;
-		
+
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		String query = "select g_board_no, title, user_name, to_char(upload_date, 'yyyyMMdd') as upload_date, readcount" +
-					" from tb_g_board" +
-					" join tb_user on (tb_g_board.uploader=tb_user.user_no)" +
-					" where group_no = ?";
+		String query = "select g_board_no, title, user_name, to_char(upload_date, 'yyyyMMdd') as upload_date, readcount"
+				+ " from tb_g_board" + " join tb_user on (tb_g_board.uploader=tb_user.user_no)" + " where group_no = ?";
 
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, groupno);
-			
+
 			rset = pstmt.executeQuery();
-			if(rset != null){
+			if (rset != null) {
 				list = new ArrayList<GBoard>();
-				while(rset.next()) {
+				while (rset.next()) {
 					GBoard gb = new GBoard();
 					gb.setgBoardNo(rset.getInt("g_board_no"));
 					gb.setTitle(rset.getString("title"));
@@ -183,25 +166,23 @@ public class GMainDao {
 		return list;
 	}
 
-
 	public ArrayList<ShareFile> selectGroupShareFile(Connection con, int groupno) {
-ArrayList<ShareFile> list = null;
-		
+		ArrayList<ShareFile> list = null;
+
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		String query = "select file_no, title, user_name, content, upload_date, originalfilename, downloadcount" +
-					" from tb_share_file" +
-					" join tb_user on (tb_share_file.uploader=tb_user.user_no)" +
-					" where group_no = ? order by file_no desc";
+		String query = "select file_no, title, user_name, content, upload_date, originalfilename, renamefilename, downloadcount"
+				+ " from tb_share_file" + " join tb_user on (tb_share_file.uploader=tb_user.user_no)"
+				+ " where group_no = ? order by file_no desc";
 
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, groupno);
-			
+
 			rset = pstmt.executeQuery();
-			if(rset != null){
+			if (rset != null) {
 				list = new ArrayList<ShareFile>();
-				while(rset.next()) {
+				while (rset.next()) {
 					ShareFile sf = new ShareFile();
 					sf.setFileNo(rset.getInt("file_no"));
 					sf.setTitle(rset.getString("title"));
@@ -209,9 +190,10 @@ ArrayList<ShareFile> list = null;
 					sf.setContent(rset.getString("content"));
 					sf.setUploadDate(rset.getDate("upload_date"));
 					sf.setFileName(rset.getString("originalfilename"));
+					sf.setRenameFileName(rset.getString("renamefilename"));
 					sf.setDownloadCount(rset.getInt("downloadcount"));
 					list.add(sf);
-					System.out.println("bbbb"+ rset.getInt("file_no"));
+
 				}
 			}
 		} catch (Exception e) {
@@ -223,5 +205,4 @@ ArrayList<ShareFile> list = null;
 		return list;
 	}
 
-	
 }
