217a05ea49c83a46bd1962fb8c796a8af5d39ad8
ydk0831 <31609765+ydk0831@users.noreply.github.com>
2017-10-25 10:13:14 +0900

    Revert "Revert "Merge branch 'master' into DK""
    This reverts commit 33b0477958314ad6246874dd36dcd7d8917a3199.

--- Project Workspace/TestProject/src/com/studyhub/group/sharefile/controller/ShareFileDeleteServlet.java
+++ Project Workspace/TestProject/src/com/studyhub/group/sharefile/controller/ShareFileDeleteServlet.java
@@ -1,6 +1,8 @@
 package com.studyhub.group.sharefile.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,35 +12,49 @@ import javax.servlet.http.HttpServletResponse;
 import com.studyhub.common.vo.ShareFile;
 import com.studyhub.group.sharefile.model.service.ShareFileService;
 
+
 /**
  * Servlet implementation class ShareFileDeleteServlet
  */
 @WebServlet("/sharefiledelete")
 public class ShareFileDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	private ShareFileService sfs;
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ShareFileDeleteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public ShareFileDeleteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+
+		int fileno = Integer.parseInt(request.getParameter("fileno"));
+
+		if (new ShareFileService().deleteShareFile(fileno) > 0) {
+			response.sendRedirect("/studyhub/sharefilelist");
+		} else {
+			RequestDispatcher view = request.getRequestDispatcher("views/group/groupFileShare/fileshareError.jsp");
+			request.setAttribute("message", "게시글 삭제 처리 실패!");
+			view.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
