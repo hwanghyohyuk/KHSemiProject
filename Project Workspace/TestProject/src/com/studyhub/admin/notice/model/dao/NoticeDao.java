ae0fbbb03781e30ac33998e2c9c9215b445521dd
hwanghyohyuk <hhhwang618@naver.com>
2017-10-30 15:19:10 +0900

    도움말notice ing
    dfdfdf

--- Project Workspace/TestProject/src/com/studyhub/admin/notice/model/dao/NoticeDao.java
+++ Project Workspace/TestProject/src/com/studyhub/admin/notice/model/dao/NoticeDao.java
@@ -5,12 +5,15 @@ import static com.studyhub.common.JDBCTemplate.close;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.studyhub.common.vo.Board;
 import com.studyhub.common.vo.Group;
 import com.studyhub.common.vo.Notice;
+import com.studyhub.common.vo.QnA;
 import com.studyhub.common.vo.UNG;
 import com.studyhub.common.vo.User;
 
@@ -22,19 +25,31 @@ public class NoticeDao {
 	private Group group;
 	private Board board;
 
-	public ArrayList<Notice> selectList(Connection conn) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
-	public HashMap<Integer, Notice> selectMap(Connection con) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Notice selectOne(Connection con, int no) {
-		// TODO Auto-generated method stub
-		return null;
+	public String selectOne(Connection con) {
+		Statement stmt = null;
+		ResultSet rset = null;
+		
+		String query = "select content from tb_notice";
+		String notice = " ";
+		try {
+			stmt = con.createStatement();
+			
+			rset = stmt.executeQuery(query);
+			while(rset.next()){
+			notice = rset.getString("content");
+			}
+			System.out.println(notice);
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			close(rset);
+			close(stmt);
+		}
+		
+		return notice;
 	}
 
 
@@ -42,7 +57,7 @@ public class NoticeDao {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		
-		String query = "insert into tb_notice values ( ? )";
+		String query = "update tb_notice set content = ?";
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, notice);
@@ -58,19 +73,26 @@ public class NoticeDao {
 	}
 
 	public int deleteNotice(Connection con, int no) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		Statement stmt = null;
+		
+		String query = "update tb_notice set content = ' ' ";
+		
+		try {
+			stmt = con.createStatement();
+			
+			result = stmt.executeUpdate(query);
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			close(stmt);
+		}
+		
+		return result;
 	}
 
-	public ArrayList<Notice> selectTitleSearch(Connection con, String keyword) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	public int updateNotice(Connection con, Notice notice) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 	
 }
 
