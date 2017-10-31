cf517fceeb639aaa7a96913c3b7c9825fed4cc9d
hwanghyohyuk <hhhwang618@naver.com>
2017-10-31 09:02:03 +0900

    메인 이미지 슬라이더

--- Project Workspace/TestProject/web/js/main.js
+++ Project Workspace/TestProject/web/js/main.js
@@ -2,3 +2,46 @@ function pleaseLogin() {
 	alert("로그인이 필요합니다.");
 	location.href = "/studyhub/login";
 }
+
+/////////////////////////////////////////////////////////////////////////////
+/////								이미지 슬라이더 							/////
+/////////////////////////////////////////////////////////////////////////////
+
+//moveType (0:left / 1:right) 
+var moveType = 0; 
+// 이동시간간격 3초 
+var moveSpeed = 1000; 
+// 움직이는 작업중 다시 명령 받지 않음 
+var moveWork = false; 
+function imgMove(move){ 
+  var moveType = move; 
+    if(moveWork==false){ 
+       // 0d\일경우 left방향 
+      if(moveType==0){ 
+         // 맨처음 이미지의 폭 
+         var aWidth = $(".RollDiv > div > a:first").width(); 
+         // 롤링마지막에 맨처음의 a태그 추가 
+         $(".RollDiv > div").append("<a href=\""+$(".RollDiv > div > a:first").attr("href")+"\">" + $(".RollDiv > div > a:first").html()+ "</a>"); 
+         // 맨처음이미지를 왼쪽으로 이동시킨다. 
+         $(".RollDiv > div > a:first").animate({marginLeft:-aWidth},{duration:moveSpeed,complete:function(){ 
+         // 이동을 마친후 첫번째 a태그를 지워버린다 
+         $(this).remove(); 
+         // 이미지 움직이는것을 다시 실행 
+         //imgMove(); 
+      }}); 
+      }else{ 
+      // 마지막 a태그의 폭 
+       var aWidth = $(".RollDiv > div > a:last").width(); 
+       // a태그 앞에 마지막의 a태그를 생성한다 단 스타일은 마지막 a태그의 폭만큼 빼준다 
+       $("<a href=\"" + $(".RollDiv > div > a:last").attr("href")+ "\" style=\"margin-left:-" + aWidth + "px\">" + $(".RollDiv > div > a:last").html()+ "</a>").insertBefore(".RollDiv > div > a:first") 
+       // 맨처음 a태그의 margin-left를 다시 0으로 맞춰준다. 
+      $(".RollDiv > div > a:first").animate({marginLeft:0},{duration:moveSpeed,complete:function(){ 
+       // 이동을 마친후 마지막 a태그를 지워버린다 
+       $(".RollDiv > div > a:last").remove(); 
+       // 이미지 움직이는것을 다시 실행 
+       //imgMove(); 
+    }}); 
+
+      }
+    }
+}
